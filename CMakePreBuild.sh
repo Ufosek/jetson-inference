5397bcefe9fc424c7b5f539b120ccc585895c7eb
Ufosek <ufoscoder@gmail.com>
2026-07-04 14:07:00 +0200

    fix ../CMakePreBuild.sh: 70: Syntax error: "fi" unexpected

--- CMakePreBuild.sh
+++ CMakePreBuild.sh
@@ -64,9 +64,9 @@ fi
 pip3 install --no-cache-dir --verbose --upgrade Cython
 
 # download/install models and PyTorch
-if [ $BUILD_CONTAINER = "NO" ]; then
+# if [ $BUILD_CONTAINER = "NO" ]; then
 	#./download-models.sh $BUILD_INTERACTIVE
 	# ./install-pytorch.sh $BUILD_INTERACTIVE
-fi
+# fi
 
 echo "[Pre-build]  Finished CMakePreBuild script"
